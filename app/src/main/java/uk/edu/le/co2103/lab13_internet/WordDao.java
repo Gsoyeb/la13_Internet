ca6a76043c80634126acdcc984dafb111ddf2038
Gsoyeb <golamsoyeb@gmail.com>
2021-04-02 11:23:30 +0100

    Add LiveData to getAlphabetizedWord() to get updated DB values

--- app/src/main/java/uk/edu/le/co2103/lab13_internet/WordDao.java
+++ app/src/main/java/uk/edu/le/co2103/lab13_internet/WordDao.java
@@ -1,5 +1,6 @@
 package uk.edu.le.co2103.lab13_internet;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -16,7 +17,7 @@ public interface WordDao {
     void deleteAll();                                       //delete all words.
 
 @Query("select * from word_table order by word asc")        //Returns a list of word in ascending order.
-    List<Word> getAlphabetizedWord();                       //method that will return a list of words in order.
+    LiveData<List<Word>> getAlphabetizedWord();             //method that will return a list of words in order. Add LiveData to constantly get the updated Database values
 }
 
 /*
@@ -25,3 +26,13 @@ public interface WordDao {
 *   Here we are going to use it for getting all words alphabetically, inserting words and deleting all words.
 *
 * */
+
+/*
+    New notes:
+        When data changed we need to take some actions. How do we do that?
+            We need to observe the data so that we can react when a change happens.
+            But observing data across multiple components can be tricky. To solve the issue we need
+            LiveData that can update data when database is updated.
+                We need to put LiveData on getAlpahbetizedWord() coz it's the only one that is needed
+                for our components to work. We need its live data.
+ */
